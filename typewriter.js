bd1e4b83bee3c8b6d229c1e752422fc45641c4d5
abdul <abdulrehman.shoukat@mgail.com>
2021-03-09 10:14:52 -0500

    type

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 let time = 0;
+let time2= 50 * sentence.length
 for (const char of sentence) {
   time += 50;
   setTimeout(() => {
         process.stdout.write(char);
   }, time)
-
 }
+setTimeout(() => {
+  process.stdout.write('\n');
+}, time2)
